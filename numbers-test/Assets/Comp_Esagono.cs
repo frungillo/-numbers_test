6dc7795693de8cecaa86f5d7204faf7aae350674
Gennaro Frungillo <g.frungillo@anm.it>
2019-09-20 20:04:01 +0200

    singolo oggetto
    devo capire come attivare l'input dell'utente sul singolo oggetto di gioco.

--- numbers-test/Assets/Comp_Esagono.cs
+++ numbers-test/Assets/Comp_Esagono.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class Comp_Esagono : MonoBehaviour
 {
-    //private SpriteRenderer sp;
+    private Animator anim;
+    public bool selezionato;
     // Start is called before the first frame update
     void Start()
     {
-      
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+      if( Input.GetMouseButton(0) && selezionato)
+        {
+            Debug.Log("Tasto Schiacciato");
+            anim.SetBool("cliccato", true);
+        } else
+        {
+            Debug.Log("Tasto NON Schiacciato");
+            anim.SetBool("cliccato", false);
+        }
     }
+
+    
 }
