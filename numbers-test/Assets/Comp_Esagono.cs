4817e607f34674f590b523df95d14a1588e61300
Gennaro Frungillo <g.frungillo@anm.it>
2019-09-20 13:30:16 +0200

    Anim
    Aggiunte animazioni

--- numbers-test/Assets/Comp_Esagono.cs
+++ numbers-test/Assets/Comp_Esagono.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class Comp_Esagono : MonoBehaviour
 {
-    private SpriteRenderer sp;
+    //private SpriteRenderer sp;
     // Start is called before the first frame update
     void Start()
     {
-        sp = GetComponent<SpriteRenderer>();
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+      
     }
 }
