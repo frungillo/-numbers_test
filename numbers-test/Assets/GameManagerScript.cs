b3f47a7630ecb200c85936568c0c1058f0ab707a
Gennaro Frungillo <g.frungillo@anm.it>
2019-09-22 13:56:04 +0200

    generazione automatica del blocchi

---
+++ numbers-test/Assets/GameManagerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public GameObject exs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float x = (float)-7.3;
+        float y = (float)3.5;
+        for (int i = 0; i < 13; i++)
+        {
+            Instantiate(
+                   exs,
+                   new Vector3(x, y, 1),
+                   Quaternion.identity);
+            if (i % 3 == 0 && i!=1) { y -= 2; x = (float)-7.3; }
+            x += (float)3.5;
+            
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
