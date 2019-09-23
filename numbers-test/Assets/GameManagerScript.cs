c1bfc14278e0d8e0ce0dd5d000e7cafa96c532e4
Gennaro Frungillo <g.frungillo@anm.it>
2019-09-23 21:01:04 +0200

    numeri

--- numbers-test/Assets/GameManagerScript.cs
+++ numbers-test/Assets/GameManagerScript.cs
@@ -10,14 +10,18 @@ public class GameManagerScript : MonoBehaviour
     {
         float x = (float)-7.3;
         float y = (float)3.5;
-        for (int i = 0; i < 13; i++)
+        for (int i = 1; i < 10; i++)
         {
             Instantiate(
                    exs,
                    new Vector3(x, y, 1),
                    Quaternion.identity);
-            if (i % 3 == 0 && i!=1) { y -= 2; x = (float)-7.3; }
-            x += (float)3.5;
+            if (i % 3 == 0 && i != 1)
+            { y -= (float)2.4; x = (float)-7.3; }
+            else
+            {
+                x += (float)2.4;
+            }
             
         }
 
