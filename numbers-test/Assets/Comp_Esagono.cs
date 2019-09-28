76ab84997d7dad858995f0c2cfb131ab35068153
Gennaro Frungillo <g.frungillo@anm.it>
2019-09-28 18:06:57 +0200

    ..

--- numbers-test/Assets/Comp_Esagono.cs
+++ numbers-test/Assets/Comp_Esagono.cs
@@ -6,38 +6,84 @@ public class Comp_Esagono : MonoBehaviour
 {
     private Animator anim;
     private AudioSource aus;
+    private SpriteRenderer spr;
+    public bool Selected;
+    public int Number;
+   
     
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         aus = GetComponent<AudioSource>();
+        spr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-
 
     }
 
     private void OnMouseOver()
     {
+
         if (Input.GetMouseButton(0))
         {
-            Debug.Log("Tasto Schiacciato");
+            //Debug.Log("Tasto Schiacciato");
             anim.SetBool("cliccato", true);
-            if (!aus.isPlaying) aus.Play();
+            if (!aus.isPlaying && !Selected)
+            {
+                Selected = true;
+                aus.Play();
+                string toAdd = "";
+                if (this.tag == "op")
+                {
+                    switch (Number)
+                    {
+                        case 1:
+                            toAdd = "*";
+                            break;
+                        case 2:
+                            toAdd = "/";
+                            break;
+                        case 3:
+                            toAdd = "-";
+                            break;
+                        case 4:
+                            toAdd = "+";
+                            break;
+                        default:
+                            break;
+                    }
+                } else { toAdd = Number.ToString(); }
+                string tots = PlayerPrefs.GetString("tots");
+                tots = tots + toAdd;
+                PlayerPrefs.SetString("tots", tots);
+                PlayerPrefs.Save();
+                if (this.tag != "op") spr.sprite = Resources.Load<Sprite>("Sprites/Exs_Numbers/" + this.Number + "_v");
+            }
         }
+         
        
     }
     private void OnMouseExit()
     {
-        Debug.Log("Tasto NON Schiacciato");
+        //Debug.Log("Tasto NON Schiacciato");
         anim.SetBool("cliccato", false);
     }
 
+  
+
+    private void OnMouseDown()
+    {
+        PlayerPrefs.SetString("Stato", "G");
+    }
+
+    private void OnMouseUp()
+    {
+        PlayerPrefs.SetString("Stato", "S");
+    }
 
 
 }
--- numbers-test/Assets/GameManagerScript.cs
+++ numbers-test/Assets/GameManagerScript.cs
@@ -1,20 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManagerScript : MonoBehaviour
 {
     public GameObject exs;
     public GameObject ops;
+    public Text txt;
+    public Text txtPunteggio;
     
+    List<GameObject> esagoniSelezionati;
+    private static GameManagerScript _instance;
+
+    public static GameManagerScript Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        } else
+        {
+            _instance = this;
+        }
+        PlayerPrefs.DeleteAll();
+        esagoniSelezionati = new List<GameObject>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+
+        
         //GameObject obj = exs.GetComponentInChildren<GameObject>();
         SpriteRenderer spr = exs.GetComponent<SpriteRenderer>(); //(SpriteRenderer)exs.GetComponentsInChildren(typeof(SpriteRenderer))[1];
         System.Random rnd = new System.Random();
         SpriteRenderer spr_op = ops.GetComponent<SpriteRenderer>(); //(SpriteRenderer)exs.GetComponentsInChildren(typeof(SpriteRenderer))[1];
         System.Random rnd_op = new System.Random();
+        
+        Comp_Esagono script_exs = exs.GetComponent<Comp_Esagono>();
+        Comp_Esagono script_ops = ops.GetComponent<Comp_Esagono>();
 
         float y = (float)5; //3.5
         
@@ -29,26 +55,30 @@ public class GameManagerScript : MonoBehaviour
             if (row % 2 == 0) { max_col_op = 5; x_op = (float)-4.99; }
             for (int col = 1; col < max_col; col++)
             {
-                int num = rnd.Next(0, 9);
+                int num = rnd.Next(0, 10);
                 spr.sprite = Resources.Load<Sprite>("Sprites/Exs_Numbers/" + num.ToString() + "_g");//GOAL!
-                Instantiate(
+                script_exs.Number = num;
+                esagoniSelezionati.Add(Instantiate(
                        exs,
                        new Vector3(x, y, 1),
                        Quaternion.identity
-                       );
+                       )
+                   );
  
                  x += (float)3.0;
             }
 
             for (int col = 1; col < max_col_op; col++)
             {
-                int num = rnd_op.Next(1, 4);
+                int num = rnd_op.Next(1, 5);
                 spr_op.sprite = Resources.Load<Sprite>("Sprites/operand/" + num.ToString());//GOAL!
-                Instantiate(
+                script_ops.Number = num;
+               esagoniSelezionati.Add( Instantiate(
                        ops,
                        new Vector3(x_op, y, 1),
                        Quaternion.identity
-                       );
+                       )
+                 );
 
                 x_op += (float)3.0;
             }
@@ -59,11 +89,34 @@ public class GameManagerScript : MonoBehaviour
 
     }
 
+
     // Update is called once per frame
     void Update()
     {
-        
+        if (PlayerPrefs.GetString("Stato") == "S") //Dito del mouse alzato
+        {
+            PlayerPrefs.DeleteAll();
+            txt.text = "";
+            foreach(GameObject itm in esagoniSelezionati)
+            {
+                Comp_Esagono scr_e = itm.GetComponent<Comp_Esagono>();
+                SpriteRenderer spr = itm.GetComponent<SpriteRenderer>();
+                scr_e.Selected = false;
+                if (itm.tag != "op") spr.sprite = Resources.Load<Sprite>("Sprites/Exs_Numbers/" + scr_e.Number.ToString() + "_g");
+            }
+        }
+        txt.text = PlayerPrefs.GetString("tots");
+        double ev = Eval(txt.text);
+        txtPunteggio.text = ev.ToString();
     }
 
+    private double Eval(string expression)
+    {
+        System.Data.DataTable table = new System.Data.DataTable();
+        table.Columns.Add("expression", string.Empty.GetType(), expression);
+        System.Data.DataRow row = table.NewRow();
+        table.Rows.Add(row);
+        return double.Parse((string)row["expression"]);
+    }
 
 }
