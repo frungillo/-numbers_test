4a053d8af67a7772a2c0b2091a95e0c82ab9067b
Genny Frungillo <g_frungillo@anm.it>
2019-11-11 15:46:59 +0100

    menu

---
+++ numbers-test/Assets/ButtonPlay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonPlay : MonoBehaviour
+{
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+   public  void TaskOnClickPlaySolo()
+    {
+        SceneManager.LoadScene("ScenaDownload");
+    }
+    
+    public void TaskOnClickMutiplayer()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
